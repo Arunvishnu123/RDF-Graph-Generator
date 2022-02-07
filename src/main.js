d1acd2b79a17a0606ebdd86267aa0c7a6ec1a57b
Arun R S <arunvishnu40@gmail.com>
2022-02-07 03:30:33 +0100

    Login Component Added

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import WaveUI from 'wave-ui'
+import 'wave-ui/dist/wave-ui.css'
+
+const app = createApp(App)
+
+new WaveUI(app, {
+    // Some Wave UI options.
+    colors: {
+        primary: '#6e9fe8',
+        secondary: '#5d9a26'
+    }
+})
+
+app.use(store).use(router).mount("#app");
