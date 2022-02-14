5e37903eafe4bb32c01efa4efa92e0d235dbaca0
Arun R S <arunvishnu40@gmail.com>
2022-02-14 04:22:33 +0100

    Protocl Specification for Peer to Peer BlockChain

--- src/components/NavBarMainComponent.vue
+++ src/components/NavBarMainComponent.vue
@@ -1,38 +1,35 @@
 <template>
-  <div>
+<div>
     <w-app>
-      <div>
-        <w-flex>
-          <w-button class="home grow" color="white" bg-color="info-dark2"
-            >Home
-          </w-button>
-        </w-flex>
-        <br />
-        <w-flex>
-          <w-button class="about grow" color="white" bg-color="info-dark2"
-            >About
-          </w-button>
-        </w-flex>
-        <w-flex>
-          <w-button class="signout grow" color="white" bg-color="info-dark2"
-            >Sign Out
-          </w-button>
-        </w-flex>
-      </div>
+        <div>
+            <w-flex>
+                <w-button class="home grow" color="white" bg-color="info-dark2">Home
+                </w-button>
+            </w-flex>
+            <br />
+            <w-flex>
+                <w-button class="about grow" color="white" bg-color="info-dark2">About
+                </w-button>
+            </w-flex>
+            <w-flex>
+                <w-button class="signout grow" color="white" bg-color="info-dark2">Sign Out
+                </w-button>
+            </w-flex>
+        </div>
     </w-app>
-  </div>
+</div>
 </template>
 
 <style scoped>
 .home {
-  top: 44px;
+    top: 44px;
 }
 
 .about {
-  top: 30px;
+    top: 30px;
 }
 
 .signout {
-  top:40px;
+    top: 40px;
 }
 </style>
